b21fdeb4e3fb828f31ee76ddbae7344240631134
dkorelov <fettqa@gmail.com>
2020-04-15 12:36:50 +0300

    orders page update

--- src/app/pages/order-pages/pages/orders-page/orders-page.component.ts
+++ src/app/pages/order-pages/pages/orders-page/orders-page.component.ts
@@ -1,21 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import {OrdersService} from "../../../../features/orders/service/orders.service";
-import {Order} from "../../../../shared/models/order-model";
-
+import {OrdersService} from '../../../../features/orders/service/orders.service';
+import {Order} from '../../../../shared/models/order-model';
+import {OrderInfo} from '../../../../shared/models/order-info-model';
 @Component({
   selector: 'app-order-page',
   templateUrl: './orders-page.component.html',
   styleUrls: ['./orders-page.component.scss']
 })
 export class OrdersPageComponent implements OnInit {
+
   orders?: Order[];
+  orderInfos?: OrderInfo[];
 
   constructor(private ordersService: OrdersService) { }
 
   ngOnInit(): void {
     this.ordersService.getOrders().subscribe(ordersList => {
-      this.orders = ordersList.items;
+      this.orders = ordersList;
+      }
+    );
+    this.ordersService.getOrdersInfo().subscribe(ordersInfo => {
+      this.orderInfos = ordersInfo.items;
     });
   }
-
 }
