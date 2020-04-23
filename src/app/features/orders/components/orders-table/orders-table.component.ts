b95e0b6f5b9a1ba42ff2904ac480124e8377541f
dkorelov <fettqa@gmail.com>
2020-04-23 03:42:01 +0300

    order-pages update

--- src/app/features/orders/components/orders-table/orders-table.component.ts
+++ src/app/features/orders/components/orders-table/orders-table.component.ts
@@ -1,5 +1,9 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 import {Order} from '../../../../shared/models/order-model';
+import {Restaurant} from '../../../restaurant/models/restaurant-model';
+import {Observable} from 'rxjs';
+import {RestaurantService} from '../../../restaurant/services/restaurant.service';
+import {element} from "protractor";
 
 
 @Component({
@@ -8,7 +12,10 @@ import {Order} from '../../../../shared/models/order-model';
   styleUrls: ['./orders-table.component.scss']
 })
 export class OrdersTableComponent implements OnInit {
+
+  constructor(public restaurantService: RestaurantService) { }
   columns = [ 'name', 'status' , 'tag'];
+  restaurants: Restaurant[];
 
   @Input()
   orders!: Order[];
@@ -19,11 +26,14 @@ export class OrdersTableComponent implements OnInit {
   selectedOrder?: Order;
   search: string;
 
-  constructor() { }
+  orderID: number;
 
   ngOnInit(): void {
-
+    this.restaurantService.getRestaurants().subscribe(restaurants => {
+      this.restaurants = restaurants;
+    });
   }
+
 
   handleOrderClick() {
     console.log('orders-table: handle order click emit in table component');
